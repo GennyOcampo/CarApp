9857ed80e78d64ac2d825f0c0d4270414ebb70a0
GennyOcampo <ogeneiva10@gmail.com>
2024-02-08 10:22:29 -0600

    Add files via upload

---
+++ MyCarApp.java
@@ -0,0 +1,32 @@
+package mycarapp;
+
+
+public class MyCarApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        BuildCar testCar1 = new BuildCar();
+        BuildCar testCar2 = new BuildCar();
+        
+        System.out.println("Status at Start\n");
+        testCar1.startTest();
+        testCar1.hitGas(50);
+        testCar1.hitBreak(10);
+        testCar1.hitGas(20);
+        testCar1.addMiles(100);
+        testCar1.addMiles(20);
+        testCar1.checkFuel_gauge(10);
+        testCar1.changeDirection(90);
+        testCar1.endTest();
+        
+        testCar2.hitGas(50);
+        testCar2.hitGas(10);
+        testCar2.hitGas(20);
+        testCar2.changeDirection(-90);
+        testCar2.changeDirection(-45);
+        testCar2.endTest();
+    }
+}
